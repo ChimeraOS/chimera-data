131a6c33985f47cf08527bceba6c8e79a6b2ec52
Alesh Slovak <alesh@playtron.one>
2024-05-24 00:38:08 -0400

    add test for game order

---
+++ tests/05-gamedb-order.sh
@@ -0,0 +1,8 @@
+cat gamedb.yaml | grep "name:" | sed 's/- name: //' | sed 's/  name: //' | sed -E 's/^"|"$//g' | sed -E "s/^'|'$//g" | sed 's/^A //I' | sed 's/^An //I' | sed 's/^The //I' > /tmp/chimera_gamedb_original_order
+cat /tmp/chimera_gamedb_original_order | sort > /tmp/chimera_gamedb_sorted_order
+
+diff -u /tmp/chimera_gamedb_original_order /tmp/chimera_gamedb_sorted_order
+if [ $? != 0 ]; then
+	echo "gamedb.yaml should be in alphabetical order by name."
+	exit 1
+fi
